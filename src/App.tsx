98ca83a96bcdd464705d91681d393848914d4bad
Miku Watanabe <mikuwatanabe@mac.lan>
2026-01-18 14:14:01 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to my Mac Portfolio
+      </h1>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
